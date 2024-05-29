4e12b249dae7af7cf0f2e949c8d079b650f7cd5c
hc <hcLenovo@qq.com>
2024-05-29 17:16:07 +0800

    init

---
+++ src/test/java/com/changgou/changgou_parent/ChanggouParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changgou.changgou_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanggouParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
